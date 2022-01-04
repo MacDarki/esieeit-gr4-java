f4d59fd3d7c398c5aa461fc5ce4c41165b86d51d
MacDarki <clem0123@hotmail.fr>
2022-01-04 16:36:38 +0100

    update Hellocontroller

--- Bibliotheque/src/main/java/com/esiee/bibliotheque/controller/HelloController.java
+++ Bibliotheque/src/main/java/com/esiee/bibliotheque/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.esiee.bibliotheque.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -7,13 +8,6 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 
     @FXML
     private TextField AuteurField;
@@ -54,5 +48,35 @@ public class HelloController {
     @FXML
     private Button btnValider;
 
+    @FXML
+    public void valider(){
+        TitreField.setText("ça fonctionne");
+    }
+
+    ///////////////////// Menu /////////////////////
+    @FXML
+    void fichierouvrir(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fichierquitter(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editionsauvegarder(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editionsauvegardersous(ActionEvent event) {
+
+    }
+
+    @FXML
+    void aboutinfos(ActionEvent event) {
+
+    }
 
 }
